46a592357e1d6eb4cef450d976abcdf285fd7560
mi_sawa <mi.sawa.1216+git@gmail.com>
2022-04-29 20:15:04 +0900

    Support multi line input

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,27 @@ lalrpop_mod!(
 fn main() -> Result<()> {
     let mut editor = Editor::<()>::new();
     editor.load_history("history.txt").ok();
+    let mut input: Option<String> = None;
     loop {
         match editor.readline(">> ") {
+            Ok(mut line) if line.ends_with('\\') => {
+                line.pop();
+                line.push('\n');
+                if let Some(input) = input.as_mut() {
+                    input.push_str(line.as_str());
+                } else {
+                    input = Some(line);
+                }
+            }
             Ok(line) => {
-                editor.add_history_entry(line.as_str());
-                match parser::TermParser::new().parse(&line) {
+                let input = if let Some(mut input) = input.take() {
+                    input.push_str(line.as_str());
+                    input
+                } else {
+                    line
+                };
+                editor.add_history_entry(input.as_str());
+                match parser::TermParser::new().parse(&input) {
                     Ok(term) => println!("{term:?}"),
                     Err(e) => eprintln!("Error: {e:?}"),
                 }
